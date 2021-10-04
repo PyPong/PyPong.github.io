e344aa8c3d5b441d7efdfc57523a778e38dd20d3
Pypong Game Git <72042143+PyPong@users.noreply.github.com>
2021-10-04 19:11:40 +0300

    Add files via upload

--- java script/navigation.js
+++ java script/navigation.js
@@ -1,39 +1,43 @@
 jQuery('document').ready(function() {
 
-	var Power = true;
+	var Power;
+
+	if ($(window).width() <= '800') {
+		Power = false;
+		$('.nav').css('width', '100%');
+		$('.nav').css('height', '29px');
+		$('.document-nav').css('display', 'none');
+	} else if ($(window).width() > '800') {
+		Power = true;
+	}
 
 	function_1 = function(){
     if (Power == true) {
-
-      $('.document-nav').css('display', 'none');
-      Power = false;
-
-      if ($(window).width() >= '800') {
+      if ($(window).width() > '800') {
         $('.nav').css('width', '60px');
         $('.nav').css('height', '40px');
-        $('.nav').css('position', 'relative');
         $('.nav').css('float', 'right');
-      } else if ($(window).width() < '800') {
+      } else if ($(window).width() <= '800') {
         $('.nav').css('width', '100%');
         $('.nav').css('height', '29px');
       }
 
+      $('.document-nav').css('display', 'none');
+      Power = false;
+
+
     } else if (Power == false) {
+
+      if ($(window).width() > '800') {
+        $('.nav').css('width', '100%');
+        $('.nav').css('height', '40px');
+      } else if ($(window).width() <= '800') {
+        $('.nav').css('height', '160px');
+        $('.nav').css('width', '100%');
+      }
 
       $('.document-nav').css('display', 'block');
       Power = true;
-
-      if ($(window).width() >= '800') {
-        $('.nav').css('width', '100%');
-        $('.nav').css('position', 'fixed');
-        $('.nav').css('height', '40px');
-      } else if ($(window).width() < '800') {
-        $('.nav').css('height', '160px');
-        $('.nav').css('width', '100%');
-        $('.nav').css('position', 'fixed');
-      }
-
-
     }
   }
 
